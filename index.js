5fa9ad2b046dedeed943a13656fb5e34abd0065a
Jovy83 <jovy83@live.com>
2021-10-02 18:06:48 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const API_KEY = 'YOURAPIKEY';
+const baseUrl = 'https://api.pexels.com/v1/search';
+
+const searchForm = document.getElementById('search-form');
+const searchTextbox = document.getElementById('search-textbox');
+const photosDiv = document.getElementById('photos-container');
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  console.log(`${baseUrl}?query=${searchTextbox.value}&orientation=square`);
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
+      var res = JSON.parse(xhttp.responseText);
+      var photoData = res.photos;
+
+      photosDiv.innerHTML = '';
+      photoData.forEach(function (photo) {
+        var photoDiv = document.createElement('div');
+        photoDiv.classList.add('photo-div');
+        photoDiv.innerHTML = `
+                            <img src=${photo.src.medium}>
+                            <h4>${photo.photographer}</h4>
+                        `;
+        photosDiv.appendChild(photoDiv);
+      });
+    }
+  };
+  xhttp.open(
+    'GET',
+    `${baseUrl}?query=${searchTextbox.value}&orientation=landscape`,
+    true
+  );
+  xhttp.setRequestHeader('Authorization', API_KEY);
+  xhttp.send();
+});
